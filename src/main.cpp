a377db587c8a75a2a35fabde356c7d600e68b9b2
nacho <nachocpol@gmail.com>
2023-01-15 17:39:48 +0000

    Added ESP32 platform. SPI works properly. This may be due to the fact that the ESP is 3V!

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,18 @@ MS5611 g_Barometer;
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println("MS5611 example program. Queries sensor every second and prints pressure (mBar) and temperature (C)");
 
     Wire.begin();
     SPI.begin();
 
     MS5611::Configuration baroConfig;
-    baroConfig.m_ChipSelectPin = 10; // D10
+    baroConfig.m_ChipSelectPin = 5; // D10 Arduino | D5 ESP32
     baroConfig.m_Mode = MS5611::Mode::M_SPI;
     baroConfig.m_CSBStatus = LOW;
-    g_Barometer.Initialize(baroConfig);   
+    g_Barometer.Initialize(baroConfig);
 }
 
 void loop()
